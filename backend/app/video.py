79aef4b2829a1971f43aeec823a106bc26ef4f5a
Elio BRUANDET <ebruandet@aubay.com>
2025-06-17 14:25:11 +0200

    fixed error when folder doesn't exist

--- backend/app/video.py
+++ backend/app/video.py
@@ -4,22 +4,20 @@ import os
 bp = Blueprint('video', __name__)
 
 UPLOAD_FOLDER = 'uploads'
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @bp.route('/upload', methods=['POST'])
 def upload():
-    if 'video' not in request.files:
-        return jsonify({'message': 'Missing Video'}), 400
-    video = request.files['video']
-    video_path = os.path.join(UPLOAD_FOLDER, video.filename)
-    video.save(video_path)
-    # Ici tu peux traiter la vidéo (ex: transcription, conversion, etc.)
+    try :
+        os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+        video = request.files['video']
+        video_path = os.path.join(UPLOAD_FOLDER, video.filename)
+        video.save(video_path)
 
-    # Puis supprimer le fichier
-    try:
-        os.remove(video_path)
-        print(f"Fichier supprimé : {video_path}")
+        try:
+            os.remove(video_path)
+        except Exception as e:
+            print(f"Erreur lors de la suppression : {e}")
+
+        return jsonify({'message': 'Translation in progress...'}), 201
     except Exception as e:
-        print(f"Erreur lors de la suppression : {e}")
-
-    return jsonify({'message': 'Translation in progress...'}), 201
+        return jsonify({'message' : 'Video not found'}), 404
