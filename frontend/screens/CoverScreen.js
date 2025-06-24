4844ba2fa57e037537a84c1980cb852385b2cd8a
Juliette LAVOCAT <jlavocat@aubay.com>
2025-06-24 17:09:35 +0200

    modif

--- frontend/screens/CoverScreen.js
+++ frontend/screens/CoverScreen.js
@@ -1,6 +1,6 @@
 // screens/CoverScreen.js
 
-import React from 'react';
+import React, { useContext } from 'react';
 import { 
   View, 
   Text, 
@@ -9,11 +9,12 @@ import {
   Alert // On le garde au cas où, mais on ne l'utilise plus pour ce bouton
 } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-
+import { AuthContext } from '../contexts/AuthContext';
 import { styles } from '../styles/coverStyles'; 
 
 export default function CoverScreen() {
   const navigation = useNavigation();
+  const { textSize } = useContext(AuthContext);
 
   const handleLoginPress = () => {
     navigation.navigate('Login');
@@ -29,13 +30,13 @@ export default function CoverScreen() {
     <SafeAreaView style={styles.container}>
       <View style={styles.buttonContainer}>
         <TouchableOpacity style={styles.loginButton} onPress={handleLoginPress}>
-          <Text style={styles.loginButtonText}>Se connecter</Text>
+          <Text style={[styles.loginButtonText, { fontSize: 16 + textSize }]}>Se connecter</Text>
         </TouchableOpacity>
 
         <TouchableOpacity onPress={handleContinueWithoutAccount}>
           <View style={styles.linkContainer}>
-            <Text style={styles.arrowText}>→</Text>
-            <Text style={styles.linkText}>Continuer sans créer de compte</Text>
+            <Text style={[styles.arrowText, { fontSize: 22 + textSize }]}>→</Text>
+            <Text style={[styles.linkText, { fontSize: 14 + textSize }]}>Continuer sans créer de compte</Text>
           </View>
         </TouchableOpacity>
         
